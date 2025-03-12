3988b852d8388f4575988328452f9155300f0adf
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-12 16:17:46 +0000

    Refactor data file

--- components/StatsContainer.tsx
+++ components/StatsContainer.tsx
@@ -7,7 +7,8 @@ import DateLabel from "./DateLabel"
 import EmptyRow from "./EmptyRow"
 import StatsRow from "./StatsRow"
 import SectionEnd from "./SectionEnd"
-import { generateSampleData } from "../utils/statsUtils"
+
+import data from "../public/data/data.json"
 
 const HeadingCell = ({ children }: { children: React.ReactNode }) => {
     return (
@@ -41,61 +42,6 @@ export default function StatsContainer({
     const containerRef = useRef<HTMLDivElement>(null)
     const [containerHeight, setContainerHeight] = useState(0)
     const [containerWidth, setContainerWidth] = useState(0)
-    const [data] = useState({
-        websites: [
-            { title: "💻 eridian.xyz", link: "https://eridian.xyz", emoji: "👀", type: "views", data: generateSampleData({ maxValue: 50 }) },
-            {
-                title: "📖 docs.eridian.xyz",
-                link: "https://docs.eridian.xyz",
-                emoji: "👀",
-                type: "views",
-                data: generateSampleData({ maxValue: 10 }),
-            },
-            {
-                title: "📋 staking.directory",
-                link: "https://staking.directory",
-                emoji: "👀",
-                type: "views",
-                data: generateSampleData({ maxValue: 200 }),
-            },
-            {
-                title: "⛺️ settlers.eridian.xyz",
-                link: "https://settlers.eridian.xyz",
-                emoji: "👀",
-                type: "views",
-                data: generateSampleData({ maxValue: 50 }),
-            },
-            {
-                title: "🏖️ pool.eridian.xyz",
-                link: "https://pool.eridian.xyz",
-                emoji: "👀",
-                type: "views",
-                data: generateSampleData({ maxValue: 20 }),
-            },
-            { title: "💰 ssvrewards.com", link: "https://ssvrewards.com", emoji: "👀", type: "views", data: generateSampleData({ maxValue: 300 }) },
-        ],
-        github: [
-            {
-                title: "💻 eridian.xyz",
-                link: "https://github.com/EridianAlpha/eridian.xyz",
-                emoji: "💻",
-                type: "commits",
-                data: generateSampleData({ maxValue: 50 }),
-            },
-            {
-                title: "📖 docs.eridian.xyz",
-                link: "https://github.com/EridianAlpha/docs",
-                emoji: "💻",
-                type: "commits",
-                data: generateSampleData({ maxValue: 10 }),
-            },
-        ],
-        socials: [
-            { title: "X Posts", link: "https://x.com/EridianAlpha", emoji: "📣", type: "posts", data: generateSampleData({ maxValue: 50 }) },
-            { title: "X Reposts", link: "https://x.com/EridianAlpha", emoji: "🔁", type: "reposts", data: generateSampleData({ maxValue: 10 }) },
-            { title: "X Comments", link: "https://x.com/EridianAlpha", emoji: "💬", type: "comments", data: generateSampleData({ maxValue: 300 }) },
-        ],
-    })
 
     useEffect(() => {
         const updateDimensions = () => {
@@ -115,8 +61,22 @@ export default function StatsContainer({
     const structureData = Object.values(data)[0][0].data
 
     return (
-        <Box ref={containerRef} bg={"contentBackground"} borderRadius={"20px"} position="relative" overflowX="scroll" maxWidth="100%">
-            <Grid templateColumns={`repeat(${structureData.length + 2}, 1fr)`} pt={3} mr={5} gap={"0px"} position="relative" w={containerWidth}>
+        <Box
+            ref={containerRef}
+            bg={"contentBackground"}
+            borderRadius={"20px"}
+            position="relative"
+            overflowX="scroll"
+            maxWidth="100%"
+        >
+            <Grid
+                templateColumns={`repeat(${structureData.length + 2}, 1fr)`}
+                pt={3}
+                mr={5}
+                gap={"0px"}
+                position="relative"
+                w={containerWidth}
+            >
                 {/* First row: Odd index dates */}
                 <Box position="sticky" left={0} zIndex={6} bg={"contentBackground"} />
                 <Box position="sticky" left={labelWidth} zIndex={3} bg={"contentBackground"} />
@@ -160,10 +120,10 @@ export default function StatsContainer({
                 {/* First Stats Rows */}
                 {Object.values(data)[0].map((config, index) => (
                     <StatsRow
-                        key={config.title}
-                        title={config.title}
+                        key={`${config.title.text}-${config.type}`}
+                        title={config.title.text}
                         link={config.link}
-                        emoji={config.emoji}
+                        emoji={config.title.emoji}
                         type={config.type}
                         data={config.data}
                         labelWidth={labelWidth}
@@ -181,13 +141,18 @@ export default function StatsContainer({
                     .map(([sectionName, section]) => (
                         <Fragment key={sectionName}>
                             <HeadingCell>{sectionName.charAt(0).toUpperCase() + sectionName.slice(1)}</HeadingCell>
-                            <EmptyRow count={structureData.length + 2} headingRow={true} height="60px" labelWidth={labelWidth} />
+                            <EmptyRow
+                                count={structureData.length + 2}
+                                headingRow={true}
+                                height="60px"
+                                labelWidth={labelWidth}
+                            />
                             {section.map((config) => (
                                 <StatsRow
-                                    key={config.title}
-                                    title={config.title}
+                                    key={`${config.title.text}-${config.type}`}
+                                    title={config.title.text}
                                     link={config.link}
-                                    emoji={config.emoji}
+                                    emoji={config.title.emoji}
                                     type={config.type}
                                     data={config.data}
                                     labelWidth={labelWidth}
@@ -200,7 +165,12 @@ export default function StatsContainer({
                         </Fragment>
                     ))}
 
-                <SectionEnd count={structureData.length + 2} labelWidth={labelWidth} selectedIndex={selectedIndex} hoverIndex={hoverIndex} />
+                <SectionEnd
+                    count={structureData.length + 2}
+                    labelWidth={labelWidth}
+                    selectedIndex={selectedIndex}
+                    hoverIndex={hoverIndex}
+                />
                 <EmptyRow count={structureData.length + 2} labelWidth={labelWidth} height="15px" />
             </Grid>
         </Box>
