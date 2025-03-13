65e07f0b4b488ebdef9660e6ebb74289ef7c9d57
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-13 13:28:36 +0000

    Add auto page refresh

--- components/ContentContainer.tsx
+++ components/ContentContainer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import { VStack, Box } from "@chakra-ui/react"
 
@@ -11,10 +11,32 @@ import Footer from "../components/Footer"
 export default function ContentContainer() {
     const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
 
+    // Refresh the page after one hour
+    // TODO: Implement scheduled refresh on data update
+    useEffect(() => {
+        const timeoutId = setTimeout(
+            () => {
+                console.log("Refreshing page after one hour")
+                window.location.reload()
+            },
+            60 * 60 * 1000, // 1 hour in milliseconds
+        )
+
+        return () => clearTimeout(timeoutId)
+    }, [])
+
     return (
         <VStack minH="100vh" onClick={() => setSelectedIndex(null)}>
             <Header />
-            <VStack alignItems={"start"} justifyContent={"center"} gap={5} w={"100%"} maxW="1400px" py={2} borderRadius={"20px"}>
+            <VStack
+                alignItems={"start"}
+                justifyContent={"center"}
+                gap={5}
+                w={"100%"}
+                maxW="1400px"
+                py={2}
+                borderRadius={"20px"}
+            >
                 <StatsContainer selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
             </VStack>
             <Box flexGrow={1} />
